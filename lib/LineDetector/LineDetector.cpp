f3c4a31fdc56d0e379afa701c27d707c1ed844aa
Tushar <tbhatt321@gmail.com>
2019-02-01 19:30:06 +0530

    Implemented library for line detection and following

---
+++ lib/LineDetector/LineDetector.cpp
@@ -0,0 +1,133 @@
+#include <Arduino.h>
+#include <LineDetector.h>
+
+// Constructor
+LineDetector::LineDetector(int pins[]) {
+    int i;
+    
+    // Assiging pins
+    for (i = 0; i < MAX_SENSORS; i++) {
+        sensors[i].pin = pins[i];
+        pinMode(pins[i], INPUT);
+    }
+
+    // Assigning weights
+    // For 8 sensors: -3 -2 -1 0 0 1 2 3
+    // Checking if total sensors are even or odd
+    bool even = !(MAX_SENSORS % 2);
+    // For even number of sensors, weight start with one less than the half of sensor count
+    // For odd number of sensors, weight starts with half of the sensor count
+    int value = even ? -(MAX_SENSORS/2 - 1) : -(MAX_SENSORS/2) ;
+    for(int i = 0; i < MAX_SENSORS; i++, value++) {
+      if(even && i == MAX_SENSORS/2)
+        // Additional condition for even sensors
+        // Two sensors in middle should have weight = 0
+        value--;
+      sensors[i].weight = value; //Adding weight to each sensor
+    }
+
+    // Calculate MAX_ERROR
+    MAX_ERROR = 0;
+    for (int i = MAX_SENSORS/2; i < MAX_SENSORS; i++)
+        MAX_ERROR += sensors[i].weight;
+
+    errSum = 0;
+    prevErr = 0;
+}
+
+// Destructor
+LineDetector::~LineDetector() {}
+
+// Calculate deviation
+int LineDetector::detect() {
+    int err = 0;
+    for (int i = 0; i < MAX_SENSORS; i++) {
+        sensors[i].value = digitalRead(sensors[i].pin);
+        // Only add error is sensor is not on line
+        if (sensors[i].value == HIGH)
+            err += sensors[i].weight;
+    }
+
+    // Return net deviation
+    return err;
+}
+
+// Calulate voltage
+int LineDetector::calcVolt(int err) {
+    // TODO tune PID constants
+    int kP = 0, kI = 0, kD = 0;
+    int P = kP * err, // Propotionality
+        D = kD * (err - prevErr); // Differential
+    errSum += err; // Integral
+    prevErr = err; // Store err for future use
+    int retval = P + (kI * errSum) + D;
+    return (retval < 0) ? -retval : retval; // Return absolute value
+}
+
+// Checks for cross-section
+bool LineDetector::isCrossSection() {
+    int sensorsOnLine = 0;
+    for (int i = 0; i < MAX_SENSORS; i++)
+        if (sensors[i].value == false) sensorsOnLine++;
+    
+    if (sensorsOnLine == MAX_SENSORS)
+        // All sensors are on the line
+        return true;
+    
+    return false;
+}
+
+// Checks for a node
+bool LineDetector::isNode() {
+    for (int i = 0; i < MAX_SENSORS; i++) {
+        if (i == 2 || i == 5) {
+            // Only sensors[2] and sensors[5] should be on the black line
+            if (sensors[i].value == true)
+                return false;
+        }
+        else if (sensors[i].value == false)
+            return false;
+    }
+
+    return true;
+}
+
+// Checks for 120 degree junction
+bool LineDetector::is120Junction() {
+    for (int i = 0; i < MAX_SENSORS; i++) {
+        if (i == 3 || i == 4) {
+            if (sensors[i].value == false)
+                // Middle sensors are on line
+                return false;
+        }
+        else if (sensors[i].value == true)
+            // Other sensors are not on line
+            return false;
+    }
+
+    return true;
+}
+
+// Check for 90 degree turns
+bool LineDetector::is90Turn() {
+    int i;
+    bool isTurn = true;
+    // Check if the first half is on line
+    for (i = 0; i < MAX_SENSORS/2; i++)
+        if (sensors[i].value == true) {
+            isTurn = false;
+            break;
+        }
+    if (!isTurn) {
+        // First half is not on line
+        // Check if second half is on line
+        isTurn = true;
+        for (; i < MAX_SENSORS; i++)
+            if (sensors[i].value == true) {
+                isTurn = false;
+                break;
+            }
+    }
+
+    return isTurn;
+}
---
+++ lib/LineDetector/LineDetector.h
@@ -0,0 +1,110 @@
+#ifndef LINE_DETECTOR_H
+#define LINE_DETECTOR_H
+
+/**
+ * Line detector library provides line following and maze solving helpers.
+ * The class interacts with the IR sensor array.
+ */
+class LineDetector{
+private:
+    // Maximum number of sensors available
+    const static short MAX_SENSORS = 8;
+
+    /**
+     * The bot currently contains a line sensor array with 8 IR sensors.
+     * However, the number of sensors are stored in a variable and not used directly in methods.
+     * Unline ultrasonic sensors and motors, change in number of IR sensors doesn't affect overall working of the bot.
+     * Each sensor has an assigned pin and a weight. It also stores the last read value.
+     */
+    struct IRSensor {
+        int pin,
+            weight;
+        bool value; // Values are digitally read, so it can be stored in boolean type.
+    } sensors[MAX_SENSORS];
+    
+    int errSum, // Sum of all caluclated errors; Used in PID
+        prevErr; // Stores last recorded error
+public:
+    // Maximum error that can be calculated by the sensor. 
+    int MAX_ERROR;
+
+    /**
+     * Constructor
+     * Initializes the sensors[] array.
+     * The weight for the pins is caluclated based on the number of sensors.
+     * It also initializes errSum and prevErr to zero.
+     * Calculted MAX_ERROR
+     * 
+     * @param pins Sensor pins in left to right sequence
+     */
+    LineDetector(int[]);
+    
+    // Destructor
+    ~LineDetector();
+    
+    /**
+     * Calculates and returns the devaition of the bot from the line.
+     * It uses the weights of the sensors and adds all the weights of the sensors which are off the line.
+     * If returned value is negative, bot is deviating to the left.
+     * If returned value is positive, bot is deviating to the right.
+     * If returned value is zero, bot is moving straight.
+     * 
+     * @return The net deviation.
+     */
+    int detect();
+
+    /**
+     * Calculates the voltage to be applied to the motors, using the error value.
+     * The error value must be calculated using the LineDetector::detect() method.
+     * 
+     * @param err The deviation of the bot
+     * @return Volage to be applied
+     */
+    int calcVolt(int);
+
+    /**
+     * Checks whether the bot is on a cross-section or not.
+     * It does that by simply checking if all sensors are on the line.
+     * The LineDetector::detect() method must be invoked before calling this method since it uses the value read by the sensors.
+     * 
+     * @return Cross-section status
+     */
+    bool isCrossSection();
+
+    /**
+     * Checks for the beginning of a node.
+     * Nodes are 6x6 cm in dimension, and the beginning of every node is marked by an OFF-ON-OFF zone.
+     * Assuming the bot is perfectly aligned, the sensors will follows this pattern
+     * Sensor     |  1 | 2  | 3   | 4  | 5  | 6   | 7  | 8
+     * -----------|----|----|-----|----|----|-----|----|---
+     * Value read | ON | ON | OFF | ON | ON | OFF | ON | ON
+     * The sensors on the extreme must read DIGITAL HIGH, this seperates node from a 120 degree junction.
+     * The LineDetector::detect() method must be invoked before calling this method since it uses the value read by the sensors.
+     * 
+     * @return Node status
+     */
+    bool isNode();
+
+    /**
+     * Checks if the bot is at a 120 degree junction.
+     * Here, the line spilts into two lines, each making 120 degrees with the other. The lines are 3cm wide.
+     * A junction will be detected if the two sensors in the center read DIGITAL HIGH while all other sensors read DIGITAL LOW.
+     * The LineDetector::detect() method must be invoked before calling this method since it uses the value read by the sensors.
+     * 
+     * @return Junciton status
+     */
+    bool is120Junction();
+
+    /**
+     * Checks whether the bot is on a 90 degree turn.
+     * A 90 degree turn must verify two conditions:
+     *  - Line sensor must record maximum error (checked externally)
+     *  - Half of the sensors must read zero value
+     * The LineDetector::detect() method must be invoked before calling this method since it uses the value read by the sensors.
+     * 
+     * @return Turn status
+     */
+    bool is90Turn();
+};
+
+#endif
