e7ffd553a1649e34aa5982445e7f32bfa627f2d5
Tushar <tbhatt321@gmail.com>
2019-02-12 21:26:34 +0530

    Impleteneted node printing on LCD

--- src/zones.cpp
+++ src/zones.cpp
@@ -1,27 +1,38 @@
 #include <Arduino.h>
+#include <Wire.h>
 #include <Globals.h>
 #include <zones.h>
 
 /**
  * Invoked when a node is found.
- * Method finds the node type, and drives the bot until the node is crossed.
+ * Method finds the node type, prints the node details and drives the bot until the node is crossed.
+ * The following data is printed:
+ *  Node: <node count>
+ *  Type: <node type>
  * 
- * @return Node type ("TRUE" or "FALSE")
+ * @param nodeCount Current number of nodes found
  */
-String getNodeType() {
+void printNode(int nodeCount) {
     String nodeType;
+    // Move until center of node is reached
     do {
         Globals::driver.move(Driver::FORWARD, 0); // Move at base volt
         Globals::line.detect(); // Updates sensor data
     }while (!Globals::line.isNode());
+    
+    // Print count and type
     nodeType = Globals::line.nodeType();
+    Globals::lcd.setCursor(6, 0);
+    Globals::lcd.print(nodeCount);
+    Globals::lcd.setCursor(6, 1);
+    Globals::lcd.print(nodeType);
+
     // Move to cross rest of the node
-    delay(2000);
+    delay(2000); // Reach last rwo of the node
     do {
         Globals::driver.move(Driver::FORWARD, 0);
         Globals::line.detect();
     }while (!Globals::line.isNode()); // Move forward until node is crossed
-    return "TRUE"; // nodeType
 }
 
 // Section 1: Maze solving and node detection
@@ -31,6 +42,13 @@ short mazeSolving(short primaryTurn) {
         //primaryTurn = Driver::LEFT, // Hand to be on the wall
         wallSide = -1; // Wall index at the end of section
     String nodeType; // "TRUE" or "FALSE"
+
+    // Initialize diplay
+    Globals::lcd.setCursor(0,0);
+    Globals::lcd.print("Node: ");
+    Globals::lcd.setCursor(0,1);
+    Globals::lcd.print("Type: ");
+
     do {
         // Get line data
         err = Globals::line.detect();
@@ -77,9 +95,8 @@ short mazeSolving(short primaryTurn) {
             else if (Globals::line.is120Junction()) Globals::driver.move(primaryTurn, volt, 60);
             // Node found
             else if (Globals::line.isNode()) {
-                nodeType = getNodeType();
                 nodeCount++;
-                // TODO Display node type and count
+                printNode(nodeCount); 
             }
             // NOTA; keep moving forward
             else Globals::driver.move(Driver::FORWARD, volt);
