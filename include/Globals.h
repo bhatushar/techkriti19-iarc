0d4342680ce17eddb9b8bd7e2b984a538dfdbfc7
Tushar <tbhatt321@gmail.com>
2019-02-12 21:26:06 +0530

    Created LiquidCrystal_I2C object

--- include/Globals.h
+++ include/Globals.h
@@ -4,12 +4,14 @@
 #include <WallDetector.h>
 #include <LineDetector.h>
 #include <Driver.h>
+#include <LiquidCrystal_I2C.h>
 
 class Globals {
 public:
     static WallDetector wall;
     static LineDetector line;
     static Driver driver;
+    static LiquidCrystal_I2C lcd;
 };
 
 #endif
