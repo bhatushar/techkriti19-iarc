afbb0a3cae48f6101ce5d07c2ad45648861aa4bf
Tushar <tbhatt321@gmail.com>
2019-02-13 21:51:13 +0530

    Initialized gloabal objects and called zone functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include <Arduino.h>
+#include <Globals.h>
+#include <zones.h>
+
+// Initialize global objects
+int usonic_pins[3][2] = {{0,0}, {0,0}, {0,0}},
+  dist_range[2] = {0, 0};
+WallDetector Globals::wall = WallDetector(usonic_pins, dist_range);
+
+int ir_pins[8] = {0,0,0,0,0,0,0,0};
+LineDetector Globals::line = LineDetector(ir_pins);
+
+int motor_pins[2][2] = {{0,0}, {0,0}};
+Driver Globals::driver = Driver(motor_pins, 100);
+
+LiquidCrystal_I2C Globals::lcd = LiquidCrystal_I2C(0x27, 16, 2);
 
 void setup() {
-  // put your setup code here, to run once:
+  byte primary = mazeSolving(Driver::LEFT);
+  wallFollowing(primary);
+  distanceMeasuring();
 }
 
 void loop() {
