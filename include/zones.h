79dc90b4c197bd397199f02149f3907519ddfe71
Tushar <tbhatt321@gmail.com>
2019-02-13 20:13:22 +0530

    Implemented wall following and distance measuring

--- include/zones.h
+++ include/zones.h
@@ -20,7 +20,26 @@
  */
 short mazeSolving(short);
 
-void wallFollowing(int);
+/**
+ * Procedure to operate bot in section B-C and C-D. The zone includes:
+ *  - Wall following
+ *  - Wall switching
+ *  - Front wall detection (Turn on opposite to primary side)
+ *  - Turn on primary side
+ * Wall is followed while maintaing an average distance, which acts as the center line between min and max distance.
+ *  
+ * @param primary Initial side on which wall is present.
+ */
+void wallFollowing(byte);
+
+/**
+ * Procedure to operate bot in section D-E. The zone includes:
+ *  - Straight line following
+ *  - Node detection
+ *  - Measuring distance between two nodes
+ * When the first node is detected, the encoder is initialised. It is stopped when the second node is detected.
+ * The encoding is done using a secondary controller which acts as slave. Distance is requested once second node is detected.
+ */
 void distanceMeasuring();
 
 #endif
--- src/zones.cpp
+++ src/zones.cpp
@@ -102,5 +102,141 @@ short mazeSolving(short primaryTurn) {
             else Globals::driver.move(Driver::FORWARD, volt);
         }
     } while (wallSide == -1); // Loop until wall is found
+
+    // Clear display
+    Globals::lcd.clear();
+
     return wallSide;
+}
+
+// Section 2: Wall following with wall switching
+void wallFollowing(byte primary) {
+    int err, volt;
+    bool completed = false;
+    do {
+        err = Globals::wall.detect(primary);
+        volt = Globals::wall.calcVolt(err);
+        
+        if (err == Globals::wall.MAX_DIST) {
+            /*
+             * No wall on primary side. It can be due to either one of the three conditions:
+             *  - Wall switched: Wall present on the opposite side of primary
+             *  - End of section reached: Determined by presence of line
+             *  - Turn on the primary side: If the above two fails, rotate bot to primary side
+             */
+            Globals::driver.stop();
+
+            // Check for wall on opposite side
+            byte side = (primary == Globals::wall.LEFT) ? Globals::wall.RIGHT : Globals::wall.LEFT;
+            if (Globals::wall.detect(side) <= Globals::wall.MAX_DIST) {
+                // Wall on opposite side present
+                // Switch primary wall
+                primary = side;
+            }
+            // No wall on opposite side; check for line
+            else if (Globals::line.detect() == 0 && Globals::line.isCrossSection()) {
+                // Line found
+                // Set completed flag
+                completed = true; // Section complete
+            }
+            // No wall, no line; turn on primary side
+            else {
+                // Move forward to properly align after turning
+                Globals::driver.move(Globals::driver.FORWARD, 0); // Move with base volt
+                // TODO measure delay
+                delay(500);
+                
+                byte turn = (primary == Globals::wall.LEFT) ? Globals::driver.RIGHT : Globals::driver.LEFT;
+                Globals::driver.move(turn, 0, 90); // Rotate with base volt
+
+                // Move to get wall on the side
+                Globals::driver.move(Globals::driver.FORWARD, 0); // Move with base volt
+                while (Globals::wall.detect(primary) >= Globals::wall.MAX_DIST) delay(200);
+                Globals::driver.stop(); // Wall reached; contuinue in next iteration
+            }
+        } else if (volt == -1) {
+            // Wall on front
+            // Turn opposite to primary
+            byte turn = (primary == Globals::wall.LEFT) ? Globals::driver.RIGHT : Globals::driver.LEFT;
+            Globals::driver.move(turn, volt, 90); // Rotate by 90 degrees
+        } else {
+            // Move bot
+            if (err < 0) {
+                // Deviation to left
+                // Move to right
+                Globals::driver.move(Globals::driver.RIGHT, volt);
+            } else if (err > 0) {
+                // Deviation to right
+                // Move to left
+                Globals::driver.move(Globals::driver.LEFT, volt);
+            } else  {
+                // No deviation
+                // Move forward
+                Globals::driver.move(Globals::driver.FORWARD, volt);
+            }
+        }
+    } while(!completed);
+}
+
+// Section 3: Measure distance between nodes
+void distanceMeasuring() {
+    int err, volt;
+    // Number of nodes encountered
+    short nodeCount = 0;
+    
+    // Do line following until two nodes are crosses
+    // This part only contains a straight line to be followed; with two TRUE nodes in between
+    do {
+        // Line Following
+        err = Globals::line.detect();
+        volt = Globals::line.calcVolt(err);
+        if (err < 0) Globals::driver.move(Driver::RIGHT, volt);
+        else if (err > 0) Globals::driver.move(Driver::LEFT, volt);
+        else Globals::driver.move(Driver::FORWARD, volt);
+        
+        // Check for node
+        if (Globals::line.isNode()) {
+            nodeCount++;
+            // First node detected
+            if (nodeCount == 1) {
+                Globals::driver.stop(); // Move after inilizing encoder
+                Globals::driver.initEncoder(); // Initialize encoder to calculate distance
+            }
+        }
+    } while (nodeCount != 3);
+    // Since the edge pattern of the node is matched, nodeCount will be 2 after crossing one node.
+    // When bot reaches the second node, nodeCount will be 3.
+    
+    // Print distance
+    Globals::driver.stop(); // Don't move or distance will be affected.
+    Globals::lcd.setCursor(0, 0);
+    Globals::lcd.print("Distance:");
+    Globals::lcd.print(Globals::driver.getDistanceTravelled(), 2);
+    Globals::lcd.print("cm");
+    Globals::driver.stopEncoder(); // Stop encoder
+
+
+    //Drive to finish line
+    bool crossSection = false;
+    do {
+        // Line Following
+        err = Globals::line.detect();
+        volt = Globals::line.calcVolt(err);
+        if (err < 0) Globals::driver.move(Driver::RIGHT, volt);
+        else if (err > 0) Globals::driver.move(Driver::LEFT, volt);
+        else {
+            Globals::driver.move(Driver::FORWARD, volt);
+            // Check for cross section here instead of inside while condition
+            // Only possible when err = 0
+            crossSection = Globals::line.isCrossSection();
+        }
+    } while (!crossSection);
+    Globals::driver.stop();
+
+    // Print finish
+    Globals::lcd.setCursor(0,1);
+    Globals::lcd.print("FINISH");
+
+    // TODO  check if necessary
+    delay (1000); // Safeguard measure
 }
